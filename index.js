9051393aa8e739a73f83fc10105a273ef03a7365
Ana Clara <anaclarasilvaxa@gmail.com>
2022-09-24 11:28:43 -0300

    Adicionado index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+var primeiroBimestre = 3
+var segundoBimestre = 6
+var terceiroBimestre = 7
+var quartoBimestre = 10
+
+if(primeiroBimestre & segundoBimestre & terceiroBimestre & quartoBimestre >= 7){
+        console.log("Aluno aprovado!")
+}else{
+        console.log("Aluno reprovado!")
+}
