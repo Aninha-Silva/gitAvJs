0508b24da47f0edff825289e4fca76aa11af081c
Aninha-Silva <101597543+Aninha-Silva@users.noreply.github.com>
2022-10-21 15:22:00 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
-var primeiroBimestre = 3
-var segundoBimestre = 6
-var terceiroBimestre = 7
-var quartoBimestre = 10
+let alunos = [{
+    id: 1,
+    nome: 'Ana',
+    bim1: 7,
+    bim2: 5, 
+    bim3: 8,
+    bim4: 9,
+    media: 7
+},
+{
+id: 2,
+    nome: 'Maria',
+    bim1: 10,
+    bim2: 10, 
+    bim3: 10,
+    bim4: 10,
+    media: 10
+}, 
+{
+id: 3,
+    nome: 'Arthur',
+    bim1: 2,
+    bim2: 6, 
+    bim3: 6,
+    bim4: 7,             
+    media: 5
+},]
 
-if(primeiroBimestre & segundoBimestre & terceiroBimestre & quartoBimestre >= 7){
-        console.log("Aluno aprovado!")
-}else{
-        console.log("Aluno reprovado!")
-}
+  alunos.forEach(notas => {
+    let media = ( notas.bim1 + notas.bim2 + notas.bim3 + notas.bim4) /4;
+    notas.media = media;
+
+    if (notas.media == 10) {
+    console.log(`${notas.id} ${notas.nome} ${notas.media} Aluno exemplar`);
+    }
+    else if (notas.media >=7) {
+    console.log(`${notas.id} ${notas.nome} ${notas.media} Aprovado`);
+    }else{
+    console.log(`${notas.id} ${notas.nome} ${notas.media} Recuperação`);
+    }
+  });
